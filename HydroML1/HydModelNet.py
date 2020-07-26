Works with alignment with hyd year-fix scaling problems

HydroML1/HydModelNet.py
@@ -4,6 +4,11 @@ from Util import *
 import numpy as np
 
 
+class Indices:
+    SLOW_STORE = 0
+    #SNOW_STORE = last store
+
+
 class HydModelNet(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, store_dim,
@@ -56,15 +61,22 @@ class HydModelNet(nn.Module):
 
     def init_stores(self, batch_size):
         self.stores = torch.zeros([batch_size, self.store_dim]).double()
-        self.stores[:, 0] = 1000
+        self.stores[:, Indices.SLOW_STORE] = 0
         self.stores[:, 1] = 100  # Start with some non-empty stores (deep, snow)
+
+    def correct_init_baseflow(self, flow, store_coeff):
+        baseflow = np.percentile(flow, 25, axis=0)  # flow is t x b
+        # Want store_coeff*slowstore = baseflow
+        self.stores[:, Indices.SLOW_STORE] = torch.from_numpy(baseflow / np.maximum(store_coeff.detach().numpy(), 0.00001))
+        print(f"Init slow store {self.stores[0, Indices.SLOW_STORE]} from baseflow {baseflow[0]}/coeff {store_coeff[0]}")
 
     #def init_hidden(self):
         # This is what we'll initialise our hidden state as
         #return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
         #        torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
-
     def forward(self, hyd_input):  # hyd_input is t x b x i
+        flow = hyd_input[:, :, 0]
+        hyd_input = hyd_input[:, :, 1:]
         idx_rain = get_indices(['prcp(mm/day)'], self.hyd_data_labels)[0]
         rain = hyd_input[:, :, idx_rain-1]  # rain is t x b
         steps = hyd_input.shape[0]
@@ -114,7 +126,6 @@ class HydModelNet(nn.Module):
                 self.stores = self.stores - escape
 
             b_flow = b[:, (-num_stores):]
-
             flow_distn = b_flow * self.stores
             self.stores = self.stores - flow_distn
 
@@ -122,6 +133,9 @@ class HydModelNet(nn.Module):
                 raise Exception("Negative store\n" + str(self.stores))
 
             flows[i, :] = flow_distn.sum(1)
+
+            if i == 0:
+                self.correct_init_baseflow(flow, b_flow[:, Indices.SLOW_STORE])
 
         if flows.min() < 0:
             raise Exception("Negative flow")

HydroML1/Hyd_ML.py
@@ -1,13 +1,13 @@
-import torch
-import torch.nn as nn
+#import torch
+#import torch.nn as nn
 from torch.utils.data import DataLoader
-import numpy as np
+#import numpy as np
 import CAMELS_data as Cd
 import os
 import math
 import matplotlib.pyplot as plt
 import time
-from HydModelNet import HydModelNet
+from HydModelNet import *
 from Util import *
 import random
 
@@ -391,9 +391,12 @@ def train_decoder_only_fakedata(decoder: HydModelNet, train_loader, input_size, 
         if hyd_data.shape[0] < batch_size:
             continue
 
+        print(f"Batch {i} of {runs}")
+
         scale_stores = i > runs/2
         numbers = list(range(hyd_data.shape[2]))
         random.shuffle(numbers)
+        numbers = numbers[1:20]
         for sample in numbers:
             inputs, inputs_no_flow = make_fake_inputs(batch_size, scale_stores, index_temp_minmax, weight_temp,
                                                       inflow_inputs, input_size, hyd_data[:, :, sample])
@@ -735,7 +738,7 @@ def run_encoder_decoder_hydmodel(decoder: HydModelNet, encoder, hyd_data, encode
     if decoder_indices is not None:
         not_decoder_indices = list(set(range(hyd_data.shape[2])) - set(decoder_indices))
         hyd_data[:, :, not_decoder_indices] = 0
-    hyd_data = hyd_data[:, :, 1:]  # drop flow [730 20 39]
+    #hyd_data = hyd_data[:, :, 1:]  # Don't drop flow; will be dropped after setting baseflow [730 20 39]
     outputs = decoder(hyd_data)  # b x t [expect
     if torch.max(np.isnan(outputs.data)) == 1:
         raise Exception('nan generated')
@@ -802,7 +805,7 @@ def train_test_everything():
     batch_size = 40
 
     train_loader, validate_loader, test_loader, input_dim, hyd_data_labels, sig_labels\
-        = load_inputs(subsample_data=200, years_per_sample=2, batch_size=batch_size)
+        = load_inputs(subsample_data=1, years_per_sample=2, batch_size=batch_size)
 
     if False:
         preview_data(train_loader, hyd_data_labels, sig_labels)