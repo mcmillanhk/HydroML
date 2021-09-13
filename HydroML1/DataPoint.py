f42a8a5941b8ac9701a66f5295a8a03a75ad0e5b
Tom Botterill <tbotterh@qti.qualcomm.com>
2021-09-12 17:56:43 -0700

    Allow attributes as encoder inputs

--- HydroML1/DataPoint.py
+++ HydroML1/DataPoint.py
@@ -2,15 +2,13 @@ import pandas as pd
 import numpy as np
 from typing import List
 import torch
-#from Util import get_indices, DatasetProperties  # Does this cause #include problems?
 
 
 class DataPoint:
     def __init__(self, gauge_id, flow_data, flow_data_cols, climate_data, climate_data_cols, signatures, attributes, latlong):
         self.gauge_id: List[str] = gauge_id
-        #self.hydro_data: pd.DataFrame = hydro_data
 
-        #Maybe flow/climate should stay as dataframes?
+        #Maybe flow/climate should stay as dataframes? Actually tensors make more sense for all input...
         self.flow_data: np.array = flow_data
         self.flow_data_cols: List[str] = flow_data_cols
         self.climate_data: np.array = climate_data
@@ -24,12 +22,6 @@ class DataPoint:
 
     def signatures_tensor(self):
         return torch.tensor(np.array(self.signatures))
-
-    #def __init__(self, datapoints):
-    #    self.gauge_id: str = [d.gauge_id for d in datapoints]
-    #    self.hydro_data: pd.DataFrame = pd.concat([d.hydro_data for d in datapoints], axis=2)
-    #    self.signatures: pd.DataFrame = pd.concat([d.signatures for d in datapoints], axis=2)
-    #    self.attributes: pd.DataFrame = pd.concat([d.attributes for d in datapoints], axis=2)
 
 
 def collate_fn(datapoints: List[DataPoint]):
